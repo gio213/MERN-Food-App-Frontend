a1440f4b8430cad8d20fdd2fd97707e1958761ab
Giorgi Patsia <gio.patsia@gmail.com>
2024-04-07 18:36:54 +0200

    now frontent in every request sends token to backend

--- src/auth/Auth0ProviderWithNavigate.tsx
+++ src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,29 +1,27 @@
-import { useCreateMyUser } from "@/api/MyUserApi";
-import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
+import { Auth0Provider } from "@auth0/auth0-react";
+import { useNavigate } from "react-router-dom";
 
 type Props = {
   children: React.ReactNode;
 };
 
 const Auth0ProviderWithNavigate = ({ children }: Props) => {
-  const { createUser } = useCreateMyUser();
+  const navigate = useNavigate();
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const cliendId = import.meta.env.VITE_AUTH0_CLIENT_ID;
   const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
-  if (!domain || !cliendId || !redirectUri) {
+  const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
+  if (!domain || !cliendId || !redirectUri || !audience) {
     throw new Error(" Unable to initialise auth0");
   }
-  const onRedirectCallback = (appState?: AppState, user?: User) => {
-    console.log("User", user);
-    if (user?.sub && user?.email) {
-      createUser({ auth0Id: user.sub, email: user.email });
-    }
+  const onRedirectCallback = async () => {
+    navigate("/auth-callback");
   };
   return (
     <Auth0Provider
       domain={domain}
       clientId={cliendId}
-      authorizationParams={{ redirect_uri: redirectUri }}
+      authorizationParams={{ redirect_uri: redirectUri, audience }}
       onRedirectCallback={onRedirectCallback}
     >
       {children}
